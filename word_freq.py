a0bbf4ba47c7cea59939df3ffb7b060caa11134e
Natali-kzn <MorozovaNatalya13@gmail.com>
2020-11-12 03:44:12 +0300

    Сделано

--- word_freq.py
+++ word_freq.py
@@ -1,12 +1,55 @@
+
 """
 Программа считает Топ-100 слов для переданного ей текстого файла.
-
 Путь до текстового файла передается программе в виде аргумента
 В выводе не должно быть стоп-слов (междометий, союзов, местоимений и т.д.)
 Список стоп-слов можно взять из популярного пакета nltk
-
 Тебе может понадобится модуль os, модуль argparse, цикл и словарь
 """
 
-if __name__ == '__main__':
-    pass
+# считываем файл, записываем в text, переводим в нижний регистр
+f = open('text.txt', "r", encoding="utf-8")
+text = f.read()
+text = text.lower()
+f.close
+# print(text)
+
+# в модуле string содержится стандартный набор символов пунктуации
+import string
+# print(string.punctuation)
+text = text.replace('\n', ' ')
+spec_chars = string.punctuation + '\xa0«»\t—…'
+
+# разделим text на символы, оставим только символы, не входящие в spec_chars, снова объединим в строку, также уберем цифры
+def remove_chars_from_text(text, chars):
+    return "".join([ch for ch in text if ch not in chars])
+
+text = remove_chars_from_text(text, spec_chars)
+# print(text)
+text = remove_chars_from_text(text, string.digits)
+# print(text)
+
+# разбиваем текст на токены (слова)
+from nltk import word_tokenize
+text_tokens = word_tokenize(text)
+
+import nltk
+text = nltk.Text(text_tokens)
+# print(text)
+
+# удалим стоп-слова
+from nltk.corpus import stopwords
+russian_stopwords = stopwords.words("russian")
+english_stopwords = stopwords.words("english")
+
+text_without_stop_words = []
+    for word in text:
+        if word not in russian_stopwords or english_stopwords:
+            text_without_stop_words.append(word)
+    return text_without_stop_words
+
+# считаем частоту слов в тексте
+from nltk.probability import FreqDist
+fdist = FreqDist(text_without_stop_words)
+fdist = fdist.most_common(100)
+print(fdist)
